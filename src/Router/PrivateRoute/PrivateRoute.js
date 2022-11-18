97356eae78b46c7a44e9f6ca172d03e2692a9a95
Khalid-Hasan96 <khbappy66@gmail.com>
2022-11-19 01:32:37 +0600

    spinner added

--- src/Router/PrivateRoute/PrivateRoute.js
+++ src/Router/PrivateRoute/PrivateRoute.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthProvider';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import { InfinitySpin } from 'react-loader-spinner';
 
-const PrivateRoute = () => {
-      return (
-            <div>
 
+const PrivateRoute = ({ children }) => {
+      const { user, loading } = useContext(AuthContext);
+      const location = useLocation();
+      if (loading) {
+            return <div className='text-center'>
+                  <InfinitySpin
+                        width='200'
+                        color="#808080"
+                  />
             </div>
-      );
+      }
+
+      if (!user) {
+            return <Navigate to='/login' state={{ from: location }} replace></Navigate>
+      }
+      return children;
 };
 
 export default PrivateRoute;
