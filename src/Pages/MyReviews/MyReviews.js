67c11e14996773424f815cf06a11f632b798bc6e
Khalid-Hasan96 <khbappy66@gmail.com>
2022-11-20 03:29:49 +0600

    review delete added

--- src/Pages/MyReviews/MyReviews.js
+++ src/Pages/MyReviews/MyReviews.js
@@ -1,51 +1,43 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../context/AuthProvider';
+import ShowMyReviews from './ShowMyReviews';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 const MyReviews = () => {
       const { user } = useContext(AuthContext);
+      const [myReviews, setMyReviews] = useState([]);
+      useEffect(() => {
+            fetch(`http://localhost:5000/clientreviews?clientEmail=${user?.email}`)
+                  .then(res => res.json())
+                  .then(data => setMyReviews(data))
+      }, [user?.email])
 
+      const handleDelete = (id) => {
+            fetch(`http://localhost:5000/reviews/${id}`, {
+                  method: 'DELETE',
+            })
+                  .then(res => res.json())
+                  .then(data => {
+                        if (data.deletedCount > 0) {
+                              alert('Deleted Successfully');
+                              const remaining = myReviews.filter(review => review._id !== id);
+                              setMyReviews(remaining)
+                        }
+                  })
+      }
 
+      console.log(myReviews)
       return (
             <div className='my-10 bg-gradient-to-r from-blue-500 to-indigo-500 rounded-2xl p-10 text-center'>
                   <h2 className='text-center text-4xl font-bold mb-5'>My Reviews</h2>
-                  <div className="bg-white bg-opacity-40 backdrop-blur-md drop-shadow-lg text-white rounded-xl mb-5 py-5">
-                        <div className="text-center">
-                              <div>
-
-                                    <p p className='mb-4'></p>
-
-                                    <div className='flex justify-around items-center'>
-                                          <div className="avatar flex justify-center items-center">
-                                                <div className="w-10 rounded-full">
-                                                      <img src={user?.photoURL} alt="" />
-                                                </div>
-                                                <p className='ml-2'>{user?.displayName}</p>
-                                          </div>
-                                          <div>
-                                                <label htmlFor="my-modal" className="btn btn-success">Edit</label>
-
-                                                <button className='ml-2 btn-outline btn btn-error'><small>Delete</small></button>
-                                          </div>
-                                    </div>
-                              </div>
-
-                        </div>
-                  </div >
-                  <input type="checkbox" id="my-modal" className="modal-toggle" />
-                  <div className="modal">
-                        <div className="modal-box">
-
-                              <form>
-                                    <textarea id="" name='addReview' className='w-3/4 p-3 border rounded-xl text-black' placeholder='Edit your Review here....'></textarea>
-                                    <br />
-
-                                    <div className="modal-action">
-                                          <label htmlFor="my-modal" className="btn btn-success">Confirm</label>
-                                    </div>
-                              </form>
-
-                        </div>
-                  </div>
+                  {
+                        myReviews.map(myReview => <ShowMyReviews
+                              key={myReview._id}
+                              myReview={myReview}
+                              handleDelete={handleDelete}
+                        ></ShowMyReviews>)
+                  }
             </div>
       );
 };
---
+++ src/Pages/MyReviews/ShowMyReviews.js
@@ -0,0 +1,53 @@
+import React, { useContext } from 'react';
+import { AuthContext } from '../../context/AuthProvider';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const ShowMyReviews = ({ myReview, handleDelete }) => {
+      const { user } = useContext(AuthContext);
+      const { _id, review, serviceTitle } = myReview;
+      console.log(review)
+      return (
+            <div>
+                  <div className="bg-white bg-opacity-40 backdrop-blur-md drop-shadow-lg text-white rounded-xl mb-5 py-5">
+                        <div className="text-center">
+
+                              <p>Service Name: <span className='text-2xl'>{serviceTitle}</span></p>
+                              <hr />
+                              <p className='my-4'>Review: <span className='text-3xl font-bold'>{review}</span></p>
+                              <div className='flex justify-around items-center'>
+                                    <div>
+                                          <label htmlFor="my-modal" className="btn btn-success">Edit</label>
+
+                                          <button onClick={() => handleDelete(_id)} className='ml-2 btn-outline btn btn-error'><small>Delete</small>
+                                          </button>
+                                          <ToastContainer />
+                                    </div>
+                              </div>
+                        </div>
+                  </div >
+
+
+
+
+
+                  <input type="checkbox" id="my-modal" className="modal-toggle" />
+                  <div className="modal">
+                        <div className="modal-box">
+
+                              <form>
+                                    <textarea id="" name='addReview' className='w-3/4 p-3 border rounded-xl text-black' placeholder='Edit your Review here....'></textarea>
+                                    <br />
+
+                                    <div className="modal-action">
+                                          <label htmlFor="my-modal" className="btn btn-success">Confirm</label>
+                                    </div>
+                              </form>
+
+                        </div>
+                  </div>
+            </div>
+      );
+};
+
+export default ShowMyReviews;
